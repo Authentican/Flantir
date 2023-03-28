00b33267e7697956e1a8945c4dcf529bbf0cb97e
Authentican <125623992+Authentican@users.noreply.github.com>
2023-03-27 22:09:04 -0400

    Add files via upload

--- index1.js
+++ index1.js
@@ -35,3 +35,17 @@ function animateOnScroll() {
     });
   });
   
+
+
+
+  window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('nav');
+    
+    if (window.scrollY > 0) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
+  
